af08896d6b83a3ed17e950ca71ef94293deb697f
sierratonin <sierramckinley1@gmail.com>
2024-02-09 17:19:31 -0500

    Practice with arithmetic

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 // let is to create a variable
 let message = "Hello";
 
-alert(message);
+//alert(message);
 
 let hello = "Hello world!";
 
@@ -11,8 +11,8 @@ let world;
 world = hello;
 
 //now both variables hold the same data.
-alert(hello);
-alert(world);
+//alert(hello);
+//alert(world);
 
 //variable can only be declared once with let,
 //after should just be referred to by variable name 
@@ -23,10 +23,27 @@ const MY_BIRTHDAY= "07.10.2001";
 let admin;
 let name = "John";
 admin = name;
-alert(admin);
+//alert(admin);
 
 let ourPlanet = "Earth";
 
 let currentVisitorName = name;
+
+//operators: +, -, *, ** (exponent), /, % (modulus, remainder), ++, --
+
+let x = 100 + 50;
+
+let a = 100;
+let b = 51;
+let x2 = a + b;
+
+let x3 = (a + 50) * a;
+
+//numbers in arithmetic are called operands (including variables)
+//operation to be performed are operators (like +, -)
+//javascript numbers are always 64-bit floating point
+//accurate up to 15 digits
+
+//const twoDecimalPlaces = lotsOfDecimal.toFixed(2); will return a number to 2 decimal places
 
 
