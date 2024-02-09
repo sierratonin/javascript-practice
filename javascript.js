9101189d794098f6906046ca5ae8f08e26889fd5
sierratonin <sierramckinley1@gmail.com>
2024-02-09 17:02:40 -0500

    Practice with variables

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// let is to create a variable
+let message = "Hello";
+
+alert(message);
+
+let hello = "Hello world!";
+
+let world;
+
+//copies "Hello world!" from hello into world
+world = hello;
+
+//now both variables hold the same data.
+alert(hello);
+alert(world);
+
+//variable can only be declared once with let,
+//after should just be referred to by variable name 
+
+//to declare a constant us const instead of let
+const MY_BIRTHDAY= "07.10.2001";
+
+let admin;
+let name = "John";
+admin = name;
+alert(admin);
+
+let ourPlanet = "Earth";
+
+let currentVisitorName = name;
+
+
