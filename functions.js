fbf0d0b695f676613bd8a060c58cf10a34b74a99
sierratonin <sierramckinley1@gmail.com>
2024-02-15 23:45:20 -0500

    Practice functions

---
+++ functions.js
@@ -0,0 +1,12 @@
+function add7(num){
+    return num + 7;
+}
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+function lastLetter(str){
+    return str.slice(str.length - 1);
+}
