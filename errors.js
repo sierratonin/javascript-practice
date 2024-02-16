1816bb56aea362a0c807e1073b2c93f99287e4af
sierratonin <sierramckinley1@gmail.com>
2024-02-16 00:08:38 -0500

    Work on troubleshooting different errors

---
+++ errors.js
@@ -0,0 +1,22 @@
+
+//Reference error, c does not exist
+
+//const a = "Hello";
+//const b = "World";
+
+//console.log(c);
+
+
+//below is to see how the stack trace works
+//const d = 5;
+//const e = 10;
+
+//function add(){
+//    return c
+//}
+//function print(){
+//    add();
+//}
+
+//print();
+
